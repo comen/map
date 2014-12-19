de6b972380d66cb725a69598c75523c09ac32f29
comen <comen.1989@gmail.com>
2014-12-19 20:48:19 +0800

    Fix timezone bug

--- src/cn/com/chinatelecom/map/servlet/ConfigListener.java
+++ src/cn/com/chinatelecom/map/servlet/ConfigListener.java
@@ -38,7 +38,7 @@ public final class ConfigListener implements ServletContextListener {
 	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
 	 */
 	public void contextInitialized(ServletContextEvent arg0) {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 		try {
 			PropertyConfigurator.configure(getClass().getResourceAsStream(
 					properties));
